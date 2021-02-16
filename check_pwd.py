367a35e0173272ed465a88c617fe29ceb0eb0a23
CDaudish <67439102+CDaudish@users.noreply.github.com>
2021-02-15 18:21:51 -0600

    Modified function to return true for all lengths between 8 and 20 inclusive

--- check_pwd.py
+++ check_pwd.py
@@ -1,4 +1,4 @@
 def check_pwd(password):
-    if len(password) == 8 or len(password) == 14 or len(password) == 20:
+    if len(password) >= 8 and len(password) <= 20:
         return True
     return False
